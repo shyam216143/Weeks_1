0576b004c2bfd73a97c2bf440126374471398015
SHYAM KUMAR <vennushyamkumar@gmail.com>
2022-03-14 16:16:58 +0530

    Problem on queue eample of insertion and removal using stack

---
+++ QueueInterfaceUsingStack.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class QueueInterfaceUsingStack {
+   Stack stack1 = new Stack<>();
+   Stack stack2 = new Stack<>();
+    public void enqueue(int element) {
+        stack1.push(element);
+        System.out.println(element + " inserted");
+    }
+    public void dequeue() {
+        if(stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        System.out.println(stack2.pop() + " removed");
+    }
+    public static void main(String[] args) {
+        QueueInterfaceUsingStack test = new QueueInterfaceUsingStack();
+        for(int i=1;i<=6;i++) {
+            test.enqueue(i);
+        }
+        test.dequeue();
+        test.dequeue();
+    }
+
+
+}
