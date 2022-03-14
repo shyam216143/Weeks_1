59327d8a3e0ba3dfbb03f0f75397e065b6929f08
SHYAM KUMAR <vennushyamkumar@gmail.com>
2022-03-14 15:22:51 +0530

    Problem on Group of integers using unique HashSet

---
+++ UniqueHashSetExample.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class UniqueHashSetExample {
+    public static void main(String[] args){
+        HashSet<Integer> hs = new HashSet<Integer>();
+        hs.add(1);
+        hs.add(1);
+        hs.add(1);
+        hs.add(4);
+        hs.add(5);
+        hs.add(1);
+        hs.add(7);
+        hs.add(3);
+        hs.add(7);
+        hs.add(6);
+        hs.add(2);
+        hs.add(9);
+        hs.add(6);
+        hs.add(0);
+        hs.add(0);
+        System.out.println("Group of integers using unique elements of HashSet  :"+hs);
+
+    }
+}
